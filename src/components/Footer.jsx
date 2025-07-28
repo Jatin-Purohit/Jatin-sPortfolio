eb988e8ac726c331ba39b53c6d228e4b2a8ebe7b
Jatin Sharma <141317836+Jatin-Purohit@users.noreply.github.com>
2025-07-28 10:25:01 +0530

    add minor change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,8 +21,8 @@ const Footer = () => {
 
   const quickLinks = [
     { name: "Home", href: "#" },
-    { name: "About", href: "#about" },
-    { name: "Projects", href: "#projects" },
+    // { name: "About", href: "#about" },
+    // { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ];
 
